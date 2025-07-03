4e9397e5ce2247196104355f8acb7eb08875f63c
abitha-peddoju12 <peddojuabhitha@gmail.com>
2025-07-03 16:01:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+virtual2 = (a,b) => {
+    console.log("Addition is" + (a + b));
+    console.log("Subtraction is" + (a - b));
+    console.log("Division is" + (a * b));
+    console.log("Muitiplication is" + (a / b));
+}
+let x = 12;
+let y = 3;
+virtual2(x,y);
